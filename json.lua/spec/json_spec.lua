c663d74e119470cad959e5d20c73bb76df28edfc
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2020-04-05 08:22:13 -0400

    Fix indent in tests

--- json.lua/spec/json_spec.lua
+++ json.lua/spec/json_spec.lua
@@ -3,21 +3,21 @@ local util = require("spec.util")
 
 describe("json", function()
    describe("encode", function()
-		it("has a proper type declaration", util.check [[
-			local json = require("json")
+      it("has a proper type declaration", util.check [[
+         local json = require("json")
 
-			local encoded: string = json.encode({ 1, 2, 3, { x = 10 } })
-			assert(encoded == '[1,2,3,{"x":10}]')
-		]])
+         local encoded: string = json.encode({ 1, 2, 3, { x = 10 } })
+         assert(encoded == '[1,2,3,{"x":10}]')
+      ]])
    end)
 
-	describe("decode", function()
-		it("has a proper type declaration", util.check [[
-			local json = require("json")
-			
-			local str = '[1,2,3,{"x":10}]'
-			local decoded: any = json.decode(str)
-			assert(json.encode(decoded) == str)
-		]])
-	end)
+   describe("decode", function()
+      it("has a proper type declaration", util.check [[
+         local json = require("json")
+
+         local str = '[1,2,3,{"x":10}]'
+         local decoded: any = json.decode(str)
+         assert(json.encode(decoded) == str)
+      ]])
+   end)
 end)
