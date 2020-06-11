0135927c356423fa86175fd46df83eb4c2d51680
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2020-06-11 19:42:34 -0400

    Don't stop check-all.sh on first failure

--- check-all.sh
+++ check-all.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-set -e
 shopt -s globstar
+
+err=0
+trap 'err=1' ERR
 
 for dir in types/*/; do
 	for declfile in $dir**/*.d.tl; do
 		tl check -I "$dir" "$declfile"
 	done
 done
+
+exit $err
