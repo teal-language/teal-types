fb5eef98cb0ddba5343bdad83146f8c6773165c9
Hisham Muhammad <hisham@gobolinux.org>
2021-01-22 22:49:40 -0300

    check-all.sh: ensure entries can find their dependencies
    Lua 5.1/JIT does not include ./?/init.lua by default in its package.path. Add
    it to ensure Teal over Lua 5.1/JIT sees ./?/init.d.tl files.

--- check-all.sh
+++ check-all.sh
@@ -5,6 +5,10 @@ shopt -s globstar
 err=0
 trap 'err=1' ERR
 
+# Lua 5.1/JIT does not include ./?/init.lua by default.
+# Add it to ensure Teal over Lua 5.1/JIT sees ./?/init.d.tl files.
+export LUA_PATH="./?.lua;./?/init.lua;$LUA_PATH"
+
 for dir in types/*/; do
 	pushd "$dir" > /dev/null
 	for declfile in **/*.d.tl; do
