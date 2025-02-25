0c9126946801521b5a48d82601adaca5ffaba8bc
mbartlett21 <mjmouse9999@gmail.com>
2025-02-26 09:15:35 +1000

    Vendor ltn12 library
    Add license for ltn12

--- scripts/deploy_luarocks.lua
+++ scripts/deploy_luarocks.lua
@@ -2,7 +2,7 @@ local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 th
 
 
 
-local ltn12 = require('ltn12')
+local ltn12 = require('vendored.ltn12')
 local lfs = require('lfs')
 
 local REPO_DIR = '..'
